d60cc49964392330737f3b2bb4308d03e05db9f4
Crowdbotics <engineering@crowdbotics.com>
2023-02-09 00:24:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-morning-de-62213",
+  url: "https://twilight-morning-de-62213.botics.co",
+  api: "https://twilight-morning-de-62213.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
